277d2612e326ebd1e566c7b0adcda2ab7bbeebd4
nauke <kkavalchuk@gmail.com>
2017-12-08 15:19:09 +0100

    Implement ResponseFilter for our REST service

---
+++ src/main/java/rest/ResponseFilter.java
@@ -0,0 +1,24 @@
+package rest;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.CacheControl;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class ResponseFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
+        MultivaluedMap<String, Object> headers = containerResponseContext.getHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Methods", "DELETE, GET, OPTIONS, POST, PUT");
+
+        CacheControl cacheControl = new CacheControl();
+        cacheControl.setNoCache(true);
+        headers.add("Cache-Control", cacheControl);
+    }
+}
